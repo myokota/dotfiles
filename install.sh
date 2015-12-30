8feef527694eb276fb3c1bcd2a417e11cf87db3f
Masashi YOKOTA <yktmss@gmail.com>
2015-12-30 21:07:48 +0000

    add setup script install.sh

---
+++ install.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+PWD=`pwd`
+
+ln -fs $PWD/zshrc ~/.zshrc
+ln -fs $PWD/tmux.conf ~/.tmux.conf
+ln -fs $PWD/../vimrc ~/.vim_runtime
+cd $PWD/../vimrc
+git checkout myself
+cd -
+ln -fs $PWD/vimrc ~/.vim_runtime/my_configs.vim
+ln -fs $PWD/vimrc ~/.vimrc
